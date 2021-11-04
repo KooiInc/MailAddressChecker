5e5dee7df7a7c8263c193302adaa8b17ff1a6070
Renzo Kooi <renzo.kooi@gmail.com>
2021-11-04 18:24:15 +0100

    Refactor/simplify

--- MailAddressChecker.js
+++ MailAddressChecker.js
@@ -18,9 +18,9 @@ export default addr => {
         .join(`|`),
   };
   const errorMsgFactory = {
-    [noParam]: () => `Please provide an email address`,
-    [moreThanOneAt]: () => `more than one @ in given address`,
-    [noDomain]: () => `no domain part (lacking @ in given address)`,
+    [noParam]: () => `*fatal* please provide an email address`,
+    [moreThanOneAt]: () => `*fatal* more than one @ in given address`,
+    [noDomain]: () => `*fatal* no domain part (lacking @ in given address)`,
     [startsOrEndsWithDot]: dl => `${dl} can't start or end with a dot (.)`,
     [doubleDot]: dl => `${dl} contains consecutive dots (.)`,
     [insufficientDomain]: dl => `${dl} part should be at least a subdomain of a top level domain`,
@@ -28,35 +28,38 @@ export default addr => {
     [invalidChrs]: dl => domain => `[${invalidChrsFound[dl](domain)}] not allowed in ${dl}`,
   };
   const createCheck = (err, msg, str2Check) => err && {
-      error: err,
-      get message() { return this.error && msg.constructor === Function ? msg(str2Check) : msg; }
-    } || {};
+    error: err,
+    get message() { return this.error && msg.constructor === Function ? msg(str2Check) : msg; }
+  } || {};
   const checkRegExps = {
     [startsOrEndsWithDot]: /\.$|^\./,
     [doubleDot]: /\.{2,}/g,
     [noValidStartChr]: /^[\p{L}]/ui,
     [invalidChrs]: {
-      l: /[^\p{L}_.#\-\d+~|=!]+/uig,
-      d: /[^\p{L}_\-.]+/ui
+      [l]: /[^\p{L}_.#\-\d+~|=!]+/uig,
+      [d]: /[^\p{L}_\-.]+/ui
     }, };
   let result = Object.entries({
     // address error checks
     [noParam]: createCheck(addr === `unknown@unknown.unknown`, errorMsgFactory[noParam]()),
     [moreThanOneAt]: createCheck(addrSplitted.length > 2, errorMsgFactory[moreThanOneAt]()),
-    [noDomain]: createCheck(!domain, errorMsgFactory[noDomain]()),
-    // local part error check
-    [startsOrEndsWithDot]: createCheck(checkRegExps[startsOrEndsWithDot].test(localPart), errorMsgFactory[startsOrEndsWithDot](l)),
-    [doubleDot]: createCheck(checkRegExps[doubleDot].test(localPart), errorMsgFactory[doubleDot](l)),
-    [noValidStartChr]: createCheck(checkRegExps[noValidStartChr].test(localPart), errorMsgFactory[noValidStartChr](l)),
-    [invalidChrs]: createCheck(checkRegExps[invalidChrs][l].test(localPart), errorMsgFactory[invalidChrs](l), localPart),
-  }).reduce( (acc, [, value]) => value.error ? [...acc, value] : acc, []);
+    [noDomain]: createCheck(!domain, errorMsgFactory[noDomain]())
+  }).reduce( (acc, [, value]) => value.error ? [...acc, value] : acc, [] );
+  // local part error check if applicable
+  result = addrSplitted.length === 2
+    ? Object.entries({
+      [startsOrEndsWithDot]: createCheck(checkRegExps[startsOrEndsWithDot].test(localPart), errorMsgFactory[startsOrEndsWithDot](l)),
+      [doubleDot]: createCheck(checkRegExps[doubleDot].test(localPart), errorMsgFactory[doubleDot](l)),
+      [noValidStartChr]: createCheck(!checkRegExps[noValidStartChr].test(localPart), errorMsgFactory[noValidStartChr](l)),
+      [invalidChrs]: createCheck(checkRegExps[invalidChrs][l].test(localPart), errorMsgFactory[invalidChrs](l), localPart)
+    }).reduce( (acc, [, value]) => value.error ? [...acc, value] : acc, result ) : result;
   // domain error checks (if applicable)
-  result = domain
+  result = domain && addrSplitted.length === 2
     ? Object.entries({
       [startsOrEndsWithDot]: createCheck(checkRegExps[startsOrEndsWithDot].test(domain), errorMsgFactory[startsOrEndsWithDot](d)),
       [doubleDot]: createCheck(checkRegExps[doubleDot].test(domain), errorMsgFactory[doubleDot](d)),
       [insufficientDomain]: createCheck(domain.split(/\./).length < 2, errorMsgFactory[insufficientDomain](d)),
-      [noValidStartChr]: createCheck(checkRegExps[noValidStartChr].test(domain), errorMsgFactory[noValidStartChr](d)),
+      [noValidStartChr]: createCheck(!checkRegExps[noValidStartChr].test(domain), errorMsgFactory[noValidStartChr](d)),
       [invalidChrs]: createCheck(checkRegExps[invalidChrs][d].test(domain), errorMsgFactory[invalidChrs](d), domain),
     }).reduce( (acc, [, value]) => value.error ? [...acc, value] : acc, result ) : result;
 
