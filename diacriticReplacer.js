52e1533ec691b95aa882aa9cfd8b617c1c5484e2
Renzo Kooi <renzo.kooi@gmail.com>
2021-11-11 10:31:32 +0100

    sz = ss

--- diacriticReplacer.js
+++ diacriticReplacer.js
@@ -107,7 +107,7 @@ function getDiacrits() {
       letters:
         '\u0053\u24C8\uFF33\u015A\u1E64\u015C\u1E60\u0160\u1E66\u1E62\u1E68\u0218\u015E\u2C7E\uA7A8\uA784',
     },
-    { base: `SZ`, letters: `\u1E9E`},
+    { base: `SS`, letters: `\u1E9E`},
     {
       base: 'T',
       letters:
@@ -227,7 +227,7 @@ function getDiacrits() {
       letters:
         '\u0073\u24E2\uFF53\u015B\u1E65\u015D\u1E61\u0161\u1E67\u1E63\u1E69\u0219\u015F\u023F\uA7A9\uA785\u1E9B',
     },
-    { base: `sz`, letters: `\u00DF` },
+    { base: `ss`, letters: `\u00DF` },
     {
       base: 't',
       letters:
